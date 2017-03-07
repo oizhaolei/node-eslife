bc751f6058d6d3f02944e1bcff263e3d7c62d540
zhaolei <oizhaolei@gmail.com>
2017-03-07 14:59:40 +0800

    JSON.parse result before return

--- test/index.js
+++ test/index.js
@@ -9,82 +9,55 @@ const eslife = new Eslife({
 
 describe('module', function () {
 
-  it('Termbyarea', function (done) {
-    eslife.termbyarea(440306).then(function (data) {
-      data = JSON.parse(data);
-      assert(data.code, 0);
-      console.log(data.data.length);
+  // it('Termbyarea', async function () {
+  //   let data = await eslife.termbyarea(440306);
+  //   assert(data.code, 0);
+  //   console.log(data.data.length);
+  // });
 
-      done();
-    });
-  });
+  // it('Termbypoint', async function () {
+  //   let data = await eslife.termbypoint(39.886089, 116.670331, 50);
+  //   assert(data.code, 0);
+  //   console.log(data.data.length);
+  // });
 
-  it('Termbypoint', function (done) {
-    eslife.termbypoint(39.886089, 116.670331, 50).then(function (data) {
-      data = JSON.parse(data);
-      assert(data.code, 0);
-      console.log(data.data.length);
+  // it('Boxtypelist', async function () {
+  //   let data = await eslife.boxtypelist('518131B048', 1);
+  //   assert(data.code, 0);
+  //   console.log(data.data);
+  // });
 
-      done();
-    });
-  });
-
-  it('Boxtypelist', function (done) {
-    eslife.boxtypelist('518131B048', 0).then(function (data) {
-      data = JSON.parse(data);
-      assert(data.code, 0);
-      console.log(data.data);
-
-      done();
-    });
-  }
-  );
-
-  it('applyrent', function (done) {
-    const overdueTime = moment().add(1, 'hours').format('YYYY-MM-DD HH:mm:ss'); // 1 hour later
-
-    const bizOrderId = `${moment().format('YYYYMMDDHHmm')}74391477109498`;
-
+  it('applyrent', async function () {
+    const overdueTime = moment().add(1, 'minutes').format('YYYY-MM-DD HH:mm:ss'); // 1 hour later
+    const bizId = `${moment().format('YYYYMMDDHHmmss')}74391477109498`;
     const opMobile = '18624357886';
     const opUser = 'zhaolei';
     const callbackUrl = 'http://wx.hearn1.com/eslife/noti';
 
-    eslife.applyrent('518131B048', 10, bizOrderId, 'medium', opMobile, opUser, overdueTime, callbackUrl).then(function(data) {
-      data = JSON.parse(data);
-      console.log('data.data', data.data);
-      assert(data.code, '0');
+    let data = await eslife.applyrent('518131B048', 10, bizId, 'small', opMobile, opUser, overdueTime, callbackUrl);
+    console.log('data.data', data.data);
+    assert(data.code, '0');
 
-      const borderId = data.data.borderId;
-      eslife.applyquery(borderId).then(function (data) {
-        data = JSON.parse(data);
-        console.log('data.data', data.data);
-        assert(data.code, '0');
+    // const borderId = data.data.borderId;
+    // data = await eslife.applyquery(borderId);
+    // console.log('data.data', data.data);
+    // assert(data.code, '0');
 
-        const actionType = 3;
-        eslife.applycancel(borderId, bizOrderId, actionType, opMobile, opUser).then(function (data) {
-          data = JSON.parse(data);
-          console.log('data.data', data.data);
-          console.log(data.data);
-
-          done();
-        });
-      });
-    });
+    // const actionType = 3;
+    // data = await eslife.applycancel(borderId, bizId, actionType, opMobile, opUser);
+    // console.log('data.data', data.data);
+    // console.log(data.data);
   });
 
-  // it('applyrecover', function (done) {
+  // it('applyrecover', async function () {
 
   //   const borderId = 'B1017022818448278845107591';
 
   //   const opMobile = '18624357886';
   //   const opUser = 'zhaolei';
 
-  //   eslife.applyrecover(borderId, opMobile, opUser, opMobile, opUser).then(function (data) {
-  //     data = JSON.parse(data);
-  //     assert(data.code, 0);
-  //     console.log(data.data);
-
-  //     done();
-  //   });
+  //   let data = await eslife.applyrecover(borderId, opMobile, opUser, opMobile, opUser);
+  //   assert(data.code, 0);
+  //   console.log(data.data);
   // });
 });
